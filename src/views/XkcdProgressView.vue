e2442df620bcabba6d370d301c7329e2be149d7a
Dawid Mucha <dawidmucha98@gmail.com>
2024-03-21 17:48:52 +0100

    viewing the comics will update them as seen

--- src/views/XkcdProgressView.vue
+++ src/views/XkcdProgressView.vue
@@ -4,41 +4,55 @@ import { useXkcdProgressStore } from '@/stores/counter'
 import { ref, onMounted } from 'vue'
 
 const store = useXkcdProgressStore()
+const progress = localStorage.getItem("xkcdTracker").split(',')
 
 const comic = ref({})
 const isSeen = ref(false)
 
 // tmp
-const lsArrayLength = ref(0)
+const lsArrayLength = ref(localStorage.getItem("xkcdTracker").split(','))
 
 onMounted(async () => {
   await store.getLatestComicNumber()
-  comic.value = store.getComic(store.latestComicNumber)
+  comic.value = await store.getComic(store.latestComicNumber)
 
   if(!localStorage.getItem("xkcdTracker")) { // if storage doesn't exist, create one
     localStorage.setItem("xkcdTracker", new Array(store.latestComicNumber))
   } else { // if storage exists
-    if(lsArrayLength.value != store.latestComicNumber) {
-      const diff = store.latestComicNumber - lsArrayLength.value
-      const diffArray = new Array(diff)
-      localStorage.setItem('xkcdTracker', ...diffArray)
+    if(lsArrayLength.value.length-1 != store.latestComicNumber) {
+      console.log('different values', lsArrayLength.value.length-1, store.latestComicNumber)
+      // TODO: finish a localStorage length correction
     }
   }
   
   lsArrayLength.value = store.latestComicNumber
-
-  if(isSeen.value == false) {
-    setTimeout(() => {
-      if(isSeen.value == false) isSeen.value = true
-    }, 1000)
-  }
+  onComicChange()
 })
 
-const markAsSeen = (e) => {
-  // e.target.checked = true
-  // const xkcdTracker = localStorage.getItem('xkcdTracker')
-  // xkcdTracker[store.comic.num] = true
-  // localStorage.setItem('xkcdTracker', xkcdTracker)
+const markAsSeen = () => {
+  // for real time update
+  progress[store.comic.num] = true
+
+  // for backend update
+  let xkcdTracker = localStorage.getItem('xkcdTracker').split(',')
+  xkcdTracker[store.comic.num] = true
+  localStorage.setItem('xkcdTracker', xkcdTracker)
+}
+
+const onComicChange = async () => {
+  if(!store.comic.num) return
+
+  await store.getComic(store.comic.num)
+  isSeen.value = progress[store.comic.num] ? true : false
+
+  if(isSeen.value === false) {
+    setTimeout(() => {
+      // a fucky wucky method to get the real time updates
+      markAsSeen()
+      isSeen.value = true
+      onComicChange()
+    }, 3000)
+  }
 }
 </script>
 
@@ -49,11 +63,18 @@ const markAsSeen = (e) => {
       <img :src="store.comic.img" alt="image of the current comic"/>
       <!-- {{ store.comic }} -->
 
-      <input type='range' min='1' :max='store.latestComicNumber' v-model="store.comic.num" @change="store.getComic(store.comic.num)" />
+      <input type='range' min='1' :max='store.latestComicNumber' v-model="store.comic.num" @change="onComicChange()" />
 
       <div>
         <label for="isSeen">Mask as seen: </label>
         <input type="checkbox" id='isSeen' name='isSeen' v-model="isSeen" @change="(e) => markAsSeen(e)" /> <span style="opacity: 0.5">(will mark as seen after 15s)</span>  
+      </div>
+
+      <div>
+        <div>Marked as seen: </div>
+        <span v-for="(progressItem, i) in progress" :key="i">
+          <span v-if="!!progressItem === true">{{ i }}, </span>
+        </span>
       </div>
     </main>
 </template>
