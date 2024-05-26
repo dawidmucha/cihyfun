a44e8a50ea860b3aeb9c35ee84f8b2418d4f7f0f
Dawid Mucha <dawidmucha98@gmail.com>
2024-05-26 21:04:15 +0200

    map tiles now react to current settings

--- src/components/xkcd/xkcdMap.vue
+++ src/components/xkcd/xkcdMap.vue
@@ -5,22 +5,51 @@ import { ref } from 'vue'
 
 const store = useXkcdStore()
 
-const ls = ref(localStorage.getItem('xkcd').split(','))
+const selectedSize = ref(0)
+const selectedFilter = ref(0)
+
+const ls = ref(localStorage.getItem('xkcd').split(',').map(Number))
 
 store.$subscribe(() => {
-  ls.value = localStorage.getItem('xkcd').split(',')
+  ls.value = localStorage.getItem('xkcd').split(',').map(Number)
+
+  switch(store.size) {
+    case "minimal": selectedSize.value = 0; break
+    case "small": selectedSize.value = 1; break
+    case "large": selectedSize.value = 2; break
+    default: console.error('store.size invalid value')
+  }
+
+  switch(store.filter) {
+    case "unseen": selectedFilter.value = 0; break
+    case "seen": selectedFilter.value = 1; break
+    case "both": selectedFilter.value = 2; break
+    default: console.error('store.filter invalid value')
+  }
 })
+
+const isComicShown = (setting, comic) => {
+  if(setting === 2) return true
+  else return (setting === comic)
+}
 </script>
 
 <template>
   <div>
     <div id="xkcdMap">
-      <XkcdSettings />
-      xkcdMap
+      <XkcdSettings :selectedSizeProp="selectedSize" :selectedFilterProp="selectedFilter" />
+      xkcdMap {{ selectedSize }} {{ selectedFilter }}
       <ol>
         <span v-for="comicNumber in store.numMax" :key="comicNumber">
-          <li v-if="ls[comicNumber] === '0'"> <!-- only unseen -->
-            <div @click="store.getComic(comicNumber)">{{ comicNumber }} {{ ls[comicNumber] === "0" ? "" : " - Seen" }}</div>
+          <li v-if="isComicShown(selectedFilter, ls[comicNumber])"> 
+            <!-- <div @click="store.getComic(comicNumber)">{{ comicNumber }} {{ ls[comicNumber] === 0 ? "" : " - Seen" }}</div>  -->
+            <div :class="{ 
+              mapTileSeen: ls[comicNumber], 
+              mapTileUnseen: !ls[comicNumber],
+              mapTileMinimal: (selectedSize === 0),
+              mapTileSmall: (selectedSize === 1),
+              mapTileLarge: (selectedSize === 2),
+            }">{{ comicNumber }}</div>
           </li>
         </span>
       </ol>
@@ -32,5 +61,28 @@ store.$subscribe(() => {
 #xkcdMap {
   height: 100%;
   overflow-y: scroll;
+}
+
+.mapTileMinimal {
+  width: 5px;
+  height: 5px;
+}
+
+.mapTileSmall {
+  width: 22px;
+  height: 22px;
+}
+
+.mapTileLarge {
+  width: 50px;
+  height: 50px;
+}
+
+.mapTileSeen {
+  background-color: green;
+}
+
+.mapTileUnseen {
+  background-color: darkred;
 }
 </style>
--- src/components/xkcd/xkcdSettings.vue
+++ src/components/xkcd/xkcdSettings.vue
@@ -1,48 +1,31 @@
 <!-- eslint-disable no-unused-vars -->
 <script setup>
 import { useXkcdStore } from '@/stores/xkcdStore'
-import { ref } from 'vue'
+import { defineProps } from 'vue'
+
+const props = defineProps(['selectedSizeProp', 'selectedFilterProp'])
 
 const store = useXkcdStore()
-
-const selectedSize = ref([0, 0, 0])
-const selectedFilter = ref([0, 0, 0])
-
-store.$subscribe(() => {
-  switch(store.size) {
-    case "large": selectedSize.value = [1, 0, 0]; break
-    case "small": selectedSize.value = [0, 1, 0]; break
-    case "minimal": selectedSize.value = [0, 0, 1]; break
-    default: console.error('store.size invalid value')
-  }
-
-  switch(store.filter) {
-    case "seen": selectedFilter.value = [1, 0, 0]; break
-    case "unseen": selectedFilter.value = [0, 1, 0]; break
-    case "both": selectedFilter.value = [0, 0, 1]; break
-    default: console.error('store.filter invalid value')
-  }
-})
 </script>
 
 <template>
   <div>
-    this diva {{ selectedSize }} {{ selectedFilter }}
+    this diva {{ props.selectedSizeProp }} {{ props.selectedFilterProp }}
     <div v-show="store.settingsCurrent === ''">
       <button @click="store.setSettingsCurrent('size')">size</button>
       <button @click="store.setSettingsCurrent('filter')">filter</button>
     </div>
     <div v-show="store.settingsCurrent === 'size'">
       <button @click="store.setSettingsCurrent('')">&lt;</button>
-      <button @click="store.settingsChange('size', 'large')" :disabled="selectedSize[0]">Large</button>
-      <button @click="store.settingsChange('size', 'small')" :disabled="selectedSize[1]">Small</button>
-      <button @click="store.settingsChange('size', 'minimal')" :disabled="selectedSize[2]">Minimal</button>
+      <button @click="store.settingsChange('size', 'large')" :disabled="selectedSizeProp[0]">Large</button>
+      <button @click="store.settingsChange('size', 'small')" :disabled="selectedSizeProp[1]">Small</button>
+      <button @click="store.settingsChange('size', 'minimal')" :disabled="selectedSizeProp[2]">Minimal</button>
     </div>
     <div v-show="store.settingsCurrent === 'filter'">
       <button @click="store.setSettingsCurrent('')">&lt;</button>
-      <button @click="store.settingsChange('filter', 'seen')" :disabled="selectedFilter[0]">Seen</button>
-      <button @click="store.settingsChange('filter', 'unseen')" :disabled="selectedFilter[1]">Unseen</button>
-      <button @click="store.settingsChange('filter', 'both')" :disabled="selectedFilter[2]">Both</button>
+      <button @click="store.settingsChange('filter', 'seen')" :disabled="selectedFilterProp[0]">Seen</button>
+      <button @click="store.settingsChange('filter', 'unseen')" :disabled="selectedFilterProp[1]">Unseen</button>
+      <button @click="store.settingsChange('filter', 'both')" :disabled="selectedFilterProp[2]">Both</button>
     </div>
   </div>
 </template>
